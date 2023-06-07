cdfd9d6d4b576722b5bba1c665dce3fff48e1cd4
rkeerthi <sunnykeerthi@gmail.com>
2023-06-07 13:07:34 +0530

    test fix

--- src/config/answersHeadlessConfig.ts
+++ src/config/answersHeadlessConfig.ts
@@ -1,8 +1,8 @@
 import { HeadlessConfig } from "@yext/search-headless-react";
 
 export const answersHeadlessConfig: HeadlessConfig = {
-  apiKey: process.env.API_KEY,
-  experienceKey: process.env.EXPERIENCE_KEY,
+  apiKey: process.env.API_KEY ?? "",
+  experienceKey: process.env.EXPERIENCE_KEY ?? "",
   locale: "en",
   // endpoints: SandboxEndpoints,
 };
